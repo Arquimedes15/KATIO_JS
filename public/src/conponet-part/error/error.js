ce7046d4d18d9a70cbb339fca9414016f11852a8
Arquimedes15 <143116307+Arquimedes15@users.noreply.github.com>
2024-02-09 00:22:32 -0500

    Delete public/src/conponet-part/error/error.js

--- public/src/conponet-part/error/error.js
+++
@@ -1,8 +0,0 @@
-var set = setTimeout(()=>{
-        
-    let s  =document.querySelector("#bt")
-    s.addEventListener("click", ()=>{
-        alert("goafadwadwadwadwadwa")
-    })
-    alert("hola")
-    }, 100)
